5980b85a34230032a8991c2c1d3fff985a15a47b
Baray Ann <annbaray66@gmail.com>
2024-10-16 20:55:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ print("Длина вашей строки: ", len(a))
 g=0
 s=0
 for i in a:
-    if i=="а" or i=="о" or i=="э" or i=="ю" or i=="я" or i=="у" or i=="е" or i=="ё" or i=="ы" or i=="и":
+    if i=="а" or i=="о" or i=="э" or i=="ю" or i=="я" or i=="у" or i=="е" or i=="ё" or i=="ы" or i=="и" or i=="А" or i=="О" or i=="Э" or i=="Ю" or i=="Я" or i=="У" or i=="Е" or i=="Ё" or i=="И":
         g+=1
     elif i!=" ":
         s+=1
